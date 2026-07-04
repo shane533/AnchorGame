18c9b9ecad92b26026063672efc80f6adcab04c3
shane533 <ptez533@gmail.com>
2026-07-05 03:49:56 +0800

    update hidden level

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'anchor-game-v3';
+const CACHE = 'anchor-game-v4';
 const ASSETS = [
     './index.html',
     './anchor.webp',
@@ -13,6 +13,14 @@ const ASSETS = [
 
 self.addEventListener('install', e => {
     e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+        )
+    );
 });
 
 self.addEventListener('fetch', e => {
